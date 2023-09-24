85c25490189f138651e13cc01f06ad2d6757eb7d
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-09-24 22:18:54 +0600

    spiral print

---
+++ 54.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int count = 0;
+        int total = row*col;
+
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row-1;
+        int endingCol = col-1;
+
+        while(count < total){
+            for(int index=startingCol; count < total && index<=endingCol; index++){
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            } startingRow++;
+
+            for(int index=startingRow; count < total && index<=endingRow; index++){
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            } endingCol--;
+
+            for(int index=endingCol; count < total && index>=startingCol; index--){
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            } endingRow--;
+
+            for(int index=endingRow; count < total && index>=startingRow; index--){
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            } startingCol++;
+        }
+        return ans;
+    }
+};
